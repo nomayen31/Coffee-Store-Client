8d30256bad8f611ce2c3e76615acc67ba1872138
nomayen <nomayen.ohin@gmail.com>
2023-12-12 21:17:52 +0600

    first commit

---
+++ src/Components/UpdateCoffee.jsx
@@ -0,0 +1,12 @@
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+             <h1 className="text-3xl font-bold text-purple-600">
+                Update Coffee
+            </h1>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
